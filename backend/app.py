96cc38fd0e40fdcf36d048882d45d4730e19bdd6
Luiz Barros <luiz_barros@trimble.com>
2022-08-06 15:22:16 -0300

    ✨ search user via UI
    🐛 Load env MUST stay before load the libraries

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,22 @@
 """
 Entry point: This file will be responsible for creating a FLASK app and configure basic routes
 """
+# pylint: disable=wrong-import-position
 
 # these imports need to be at top because it will load the env files for the other methods
 import os
 
 from dotenv import load_dotenv
+load_dotenv(dotenv_path=os.path.join(f'{os.getcwd()}', '.env'))
 
 from src.controllers.userController import UserController
 from src.controllers.userProductController import UserProductController
-load_dotenv(dotenv_path=os.path.join(f'{os.getcwd()}', '.env'))
 
-# pylint: disable=wrong-import-position
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS  # type: ignore
 
 from src.controllers.productController import ProductController
-# pylint: enable=wrong-import-position
+
 
 LISTEN_HOST = os.environ.get("KPN_LISTEN_HOST") or "localhost"
 LISTEN_PORT = os.environ.get("KPN_LISTEN_PORT") or 5010
